91f60e6260996d5ee7dae26672f016ece17ae957
siva <sivapriyan2899@gmail.com>
2024-07-08 07:11:28 +0530

    Adding Lab002

--- src/ex_07072024/Lab002.java
+++ src/ex_07072024/Lab002.java
@@ -1,7 +1,7 @@
 package ex_07072024;
 
-public class Lab001 {
+public class Lab002 {
     public static void main(String[] args) {
-        System.out.println("Hellow world");
+        System.out.println("Hello Pramod !!!");
     }
 }
