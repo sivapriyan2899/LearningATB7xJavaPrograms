f074849b2c9eb4d58a4db5f18a45983b0ea66f16
siva <sivapriyan2899@gmail.com>
2024-07-08 07:12:03 +0530

    Adding Lab003 and Lab004

---
+++ src/ex_07072024/Lab003.java
@@ -0,0 +1,17 @@
+package ex_07072024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        //single line comment - this will not execute
+
+        /**
+         * multiple line comment
+         *
+         *
+         *
+         *
+         *
+         */
+
+    }
+}
---
+++ src/ex_07072024/Lab004.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+public class Lab004 {
+
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+    }
+}
